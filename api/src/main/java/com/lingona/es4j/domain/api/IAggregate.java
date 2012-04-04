5826fd3d94b40be86cd876022f58636228cc05fd
esfand <esfand@wow.com>
2012-04-04 14:43:52 +0300

    Switched PCs.

--- api/src/main/java/com/lingona/es4j/domain/api/IAggregate.java
+++ api/src/main/java/com/lingona/es4j/domain/api/IAggregate.java
@@ -16,5 +16,25 @@ public interface IAggregate {
 
     IMemento getSnapshot();
 
-    void throwHandlerNotFound(Object eventMessage);
+//  void throwHandlerNotFound(Object eventMessage);
 }
+
+/*
+namespace CommonDomain
+{
+	using System;
+	using System.Collections;
+
+	public interface IAggregate
+	{
+		Guid Id { get; }
+		int Version { get; }
+
+		void ApplyEvent(object @event);
+		ICollection GetUncommittedEvents();
+		void ClearUncommittedEvents();
+
+		IMemento GetSnapshot();
+	}
+}
+*/
